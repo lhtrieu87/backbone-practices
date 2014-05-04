b90eed83e6fd7f522b29154d8ddfc5641dc39394
Le Hong Trieu <pair+trieu@sabaton.com>
2014-05-04 12:36:24 +0800

    Models

---
+++ Todo-Marionette/js/todos.js
@@ -0,0 +1,47 @@
+'use strict';
+
+App.module('Todos', function(Todos, App, Backbone) {
+	Todos.Todo = Backbone.Model.extend({
+		defaults: {
+			title: '',
+			completed: false,
+			created: 0
+		},
+
+		initialize: function() {
+			if(this.isNew()) {
+				this.set('created', Date.now());
+			}
+		},
+
+		toggle: function() {
+			return this.set('completed', !this.isCompleted());
+		},
+
+		isCompleted: function() {
+			return this.get('completed');
+		}
+	});
+
+	Todos.TodoList = Backbone.Collection.extend({
+		model: Todos.Todo,
+
+		localStorage: new Backbone.LocalStorage('todos-backbone-marionette'),
+
+		getCompleted: function() {
+			return this.filter(this._isCompleted);
+		},
+
+		getActive: function() {
+			return this.reject(this._isCompleted);
+		},
+
+		comparator: function(todo) {
+			return todo.get('created');
+		},
+
+		_isCompleted: function(todo) {
+			return todo.isCompleted();
+		}
+	});
+});
