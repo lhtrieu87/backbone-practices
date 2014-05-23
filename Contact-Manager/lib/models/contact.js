6b6a8e49fb83bad8c99c398b21767fafc4fc9ecb
Le Hong Trieu <lhtrieu87@gmail.com>
2014-05-23 09:01:21 +0800

    Contact entity
    - Underscore.String

---
+++ Contact-Manager/lib/models/contact.js
@@ -0,0 +1,27 @@
+define(['backbone', 'underscore', 'underscore.string'], function (Backbone, _) {
+  var Contact = Backbone.Model.extend({
+    defaults: {
+      firstName: "",
+      lastName: "",
+      phoneNumber: ""
+    },
+
+    validate: function (attrs, options) {
+      var errors = {};
+
+      if(_.str.isBlank(attrs.firstName)) {
+        errors.firstName = 'Firstname cannot be blank!';
+      }
+
+      if(_.str.isBlank(attrs.lastName)) {
+        errors.lastName = 'Lastname cannot be blank!';
+      }
+
+      if(!_.isEmpty(errors)) {
+        return errors;
+      }
+    }
+  });
+
+  return Contact;
+});
